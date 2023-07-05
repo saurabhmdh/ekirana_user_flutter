7782bdc17f908020d75860064b973a8767495f00
samridhi <samridhi102kri@gmail.com>
2023-07-05 16:39:33 +0530

    product list screen completed

--- lib/screens/ProductListScreen.dart
+++ lib/screens/ProductListScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg_provider/flutter_svg_provider.dart';
-
+import 'demo_list.dart';
+//ignore: must_be_immutable
 class ProductListScreen extends StatefulWidget{
   String title;
   ImageProvider<Object> icon;
@@ -9,7 +10,7 @@ class ProductListScreen extends StatefulWidget{
   _ProductListScreen createState() => _ProductListScreen(title,icon);
 }
 class _ProductListScreen extends State<ProductListScreen> {
-
+  List myList=demoList;
   String title;
   ImageProvider<Object> icon;
   _ProductListScreen(this.title,this.icon);
@@ -60,6 +61,89 @@ class _ProductListScreen extends State<ProductListScreen> {
             ),
           ),
         ),
+        body: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 8,left: 8,right: 8),
+                child: TextField(
+                  textAlignVertical: TextAlignVertical.center,
+                  cursorColor: Colors.black,
+                  textAlign: TextAlign.left,
+                  decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide(
+                              color: Colors.grey.shade300,
+                              width: 1
+                          )
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide(
+                              color: Colors.grey.shade300,
+                              width: 1
+                          )
+                      ),
+                      suffixIcon: Icon(Icons.search,color: Colors.green.shade800,),
+                      hintText: "Search",
+                      filled: true,
+                      fillColor: Colors.white,
+                      contentPadding: EdgeInsets.only(left: 16)
+                  ),
+                ),
+              ),
+              SizedBox(height: 8,),
+              Expanded(
+                child: GridView.builder(gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 8,
+                  crossAxisSpacing: 8,
+                  childAspectRatio: 155/260,
+                ),itemCount: myList.length, itemBuilder: (BuildContext context, int index){
+                  return Card(
+                    elevation: 4,
+                          child: Container(
+                            child: Column(
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.all(6.0),
+                                  child: Image(image: NetworkImage(demoList[index]['item_image']),width: 143,height: 143,),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.only(left: 8,right: 8,top: 4),
+                                  child: Text(demoList[index]['item_name'],style: TextStyle(fontWeight: FontWeight.w600,color: Colors.black, fontSize: 14),textAlign: TextAlign.center,)
+                                ),
+                                Padding(
+                                    padding: EdgeInsets.only(left: 8,right: 8,top: 4),
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        Text("\$"+demoList[index]['original_price'],style: TextStyle(fontWeight: FontWeight.w600,color: Colors.grey.shade600, fontSize: 12),textAlign: TextAlign.center,),
+                                        Text("\$"+demoList[index]['discount_price'],style: TextStyle(fontWeight: FontWeight.w600,color: Colors.green.shade800, fontSize: 14),textAlign: TextAlign.center,),
+                                      ],
+                                    )
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(30),
+                                        color: Colors.orange
+                                    ),
+                                    child: const Padding(
+                                      padding: EdgeInsets.only(left: 16,right: 16,top: 8,bottom: 8),
+                                      child: Text("Add to Cart",style: TextStyle(fontWeight: FontWeight.w600,color: Colors.white, fontSize: 14),textAlign: TextAlign.center,),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                  );
+                }),
+              ),
+            ],
+          ),
       ),
     );
   }
---
+++ lib/screens/demo_list.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+final List<Map<String,dynamic>> demoList=[
+  {
+  "item_image":"https://www.freepnglogos.com/uploads/vegetables-png/fruit-and-vegetables-basket-png-20.png",
+  "item_name":"Vegetable",
+  "original_price":"300.00",
+  "discount_price": "200.00",
+},
+  {
+    "item_image":"https://img.shpy.in/71412/1636360707979_SKU-0896_0.jpeg",
+    "item_name":"Atta",
+    "original_price":"500.00",
+    "discount_price": "300.00",
+  },
+  {
+    "item_image":"https://www.efghfoods.com/wp-content/uploads/2022/06/general_544923805.jpg",
+    "item_name":"Masala",
+    "original_price":"200.00",
+    "discount_price": "150.00",
+  },
+  {
+    "item_image":"https://www.healthyeating.org/images/default-source/home-0.0/nutrition-topics-2.0/general-nutrition-wellness/2-2-2-3foodgroups_fruits_detailfeature.jpg",
+    "item_name":"Fruits",
+    "original_price":"300",
+    "discount_price": "200",
+  },
+  {
+    "item_image":"https://www.freepnglogos.com/uploads/vegetables-png/fruit-and-vegetables-basket-png-20.png",
+    "item_name":"Vegetable",
+    "original_price":"300.00",
+    "discount_price": "200.00",
+  },
+  {
+    "item_image":"https://img.shpy.in/71412/1636360707979_SKU-0896_0.jpeg",
+    "item_name":"Atta",
+    "original_price":"500.00",
+    "discount_price": "300.00",
+  },
+  {
+    "item_image":"https://www.efghfoods.com/wp-content/uploads/2022/06/general_544923805.jpg",
+    "item_name":"Masala",
+    "original_price":"200.00",
+    "discount_price": "150.00",
+  },
+  {
+    "item_image":"https://www.healthyeating.org/images/default-source/home-0.0/nutrition-topics-2.0/general-nutrition-wellness/2-2-2-3foodgroups_fruits_detailfeature.jpg",
+    "item_name":"Fruits",
+    "original_price":"300",
+    "discount_price": "200",
+  },
+  {
+    "item_image":"https://www.freepnglogos.com/uploads/vegetables-png/fruit-and-vegetables-basket-png-20.png",
+    "item_name":"Vegetable",
+    "original_price":"300.00",
+    "discount_price": "200.00",
+  },
+  {
+    "item_image":"https://img.shpy.in/71412/1636360707979_SKU-0896_0.jpeg",
+    "item_name":"Atta",
+    "original_price":"500.00",
+    "discount_price": "300.00",
+  },
+  {
+    "item_image":"https://www.efghfoods.com/wp-content/uploads/2022/06/general_544923805.jpg",
+    "item_name":"Masala",
+    "original_price":"200.00",
+    "discount_price": "150.00",
+  },
+  {
+    "item_image":"https://www.healthyeating.org/images/default-source/home-0.0/nutrition-topics-2.0/general-nutrition-wellness/2-2-2-3foodgroups_fruits_detailfeature.jpg",
+    "item_name":"Fruits",
+    "original_price":"300",
+    "discount_price": "200",
+  },
+  {
+    "item_image":"https://www.freepnglogos.com/uploads/vegetables-png/fruit-and-vegetables-basket-png-20.png",
+    "item_name":"Vegetable",
+    "original_price":"300.00",
+    "discount_price": "200.00",
+  },
+  {
+    "item_image":"https://img.shpy.in/71412/1636360707979_SKU-0896_0.jpeg",
+    "item_name":"Atta",
+    "original_price":"500.00",
+    "discount_price": "300.00",
+  },
+  {
+    "item_image":"https://www.efghfoods.com/wp-content/uploads/2022/06/general_544923805.jpg",
+    "item_name":"Masala",
+    "original_price":"200.00",
+    "discount_price": "150.00",
+  },
+  {
+    "item_image":"https://www.healthyeating.org/images/default-source/home-0.0/nutrition-topics-2.0/general-nutrition-wellness/2-2-2-3foodgroups_fruits_detailfeature.jpg",
+    "item_name":"Fruits",
+    "original_price":"300",
+    "discount_price": "200",
+  },
+];
